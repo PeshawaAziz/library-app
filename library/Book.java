d3f1ace108ce6d6dcb95c2dc5dc9b96a203aca9b
PeshawaAziz <modern3p2017@gmail.com>
2025-03-18 00:55:50 +0330

    feat: implement Library class

--- library/Book.java
+++ library/Book.java
@@ -10,15 +10,14 @@ public class Book {
     }
 
     public void setTitle(String title) {
-        if (title == null || title.isEmpty()) {
-            throw new IllegalArgumentException("Title cannot be empty");
-        }
+        if (title == null || title.isEmpty())
+            throw new IllegalArgumentException("Title cannot be empty.");
         this.title = title;
     }
 
     public void setPageCount(int pageCount) {
         if (pageCount <= 0)
-            throw new IllegalArgumentException("Page count must be positive");
+            throw new IllegalArgumentException("Page count must be positive.");
         this.pageCount = pageCount;
     }
 
